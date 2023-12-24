e00d72093a813d7bfe7b4808b3db14ef8d12321b
GaryDolan <garydolan4@gmail.com>
2023-12-24 01:50:02 +0000

    Add home page template contents and style #50

--- home/views.py
+++ home/views.py
@@ -5,6 +5,6 @@ from blog.models import Post
 class IndexView(generic.View):
     
     def get(self, request):
-        recent_posts = Post.objects.filter(status=1).order_by('-created_on')[:3]
+        recent_posts = Post.objects.filter(status=1).order_by('-created_on')[:4]
         current_page = 'home'
         return render(request, 'index.html',{'recent_posts': recent_posts, 'current_page': current_page})
