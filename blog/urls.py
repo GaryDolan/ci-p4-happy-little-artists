41c40e24965897fef6e2f816e3b20af336c4c5e3
GaryDolan <garydolan4@gmail.com>
2023-12-29 00:11:52 +0000

    Add like/unlike posts #31

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostLike
 
 
 urlpatterns = [
     path('club_news/', PostListView.as_view(), name='club_news'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('like/<slug:slug>/', PostLike.as_view(), name='post_like'),
 ]
