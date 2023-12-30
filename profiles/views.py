c76e1e70f335fee641d948177784144cb0cc9874
GaryDolan <garydolan4@gmail.com>
2023-12-30 02:36:02 +0000

    Update profile view, template and style #43

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Profile
@@ -8,6 +9,24 @@ class ProfileDetailView(generic.DetailView):
     context_object_name = "profile"
 
     def get_object(self, queryset=None):
+        # Get the username from the URL
         username = self.kwargs.get('username')
-        return get_object_or_404(Profile, user__username=username)
+
+        # Find the user object with username in URL 
+        user = get_object_or_404(User, username=username)
+
+        # Find the profile with username attached 
+        return get_object_or_404(Profile, user=user) # self.object is now the user profile of given username
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Set the flag (highlights profile tab in nav) but only if its your profile
+        context['current_page'] = 'profile' if self.request.user == self.object.user else None
+
+        #Get users liked posts
+        context['liked_posts'] = self.object.user.blog_likes.all()
+
+
+        return context
 
