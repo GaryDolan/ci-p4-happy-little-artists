e1e87609d5c902f50a94f9a0e98f6a1a259a866e
GaryDolan <garydolan4@gmail.com>
2024-01-01 23:50:26 +0000

    Add art class admin class #45

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ArtClass
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(ArtClass)
+class ArtClassAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'duration', 'location', 'age_group', 'bookings_count', 'max_bookings')
+    search_fields = ('title', 'location')
+    list_filter = ('age_group','location')
+    
