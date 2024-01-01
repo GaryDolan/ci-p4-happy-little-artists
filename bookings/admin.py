5458c3cb6e4d4330ddbe3e90a37ec5e779226fbe
GaryDolan <garydolan4@gmail.com>
2024-01-01 23:51:06 +0000

    Add booking admin class #45

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ArtClass
+from .models import ArtClass, Booking
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -9,3 +9,20 @@ class ArtClassAdmin(admin.ModelAdmin):
     search_fields = ('title', 'location')
     list_filter = ('age_group','location')
     
+@admin.register(Booking)
+class BookingAdmin(SummernoteModelAdmin):
+
+    list_display = ('owner', 'child_name', 'art_class', 'payment_status', 'date_created')
+    list_filter = ('owner', 'child_name', 'art_class__title', 'payment_status','date_created')
+    search_fields = ['owner__username', 'child_name']
+    summernote_fields = ('admin_notes',)
+    
+    actions = ['mark_as_paid', 'mark_as_not_paid']
+
+    def mark_as_not_paid(self, request, queryset):
+        queryset.update(payment_status=0)
+        self.message_user(request, f'selected bookings have been marked as not paid.')
+
+    def mark_as_paid(self, request, queryset):
+        queryset.update(payment_status=1)
+        self.message_user(request, f'selected bookings have been marked as paid.')
