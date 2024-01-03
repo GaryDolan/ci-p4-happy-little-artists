4baaaf3bd27d6818ba1b049b4767005d697b706c
GaryDolan <garydolan4@gmail.com>
2024-01-03 18:05:38 +0000

    Add delete booking links and URL #47

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateBookingView, EditBookingView
+from .views import CreateBookingView, EditBookingView, DeleteBookingView
 
 
 urlpatterns = [
     path('create_booking/', CreateBookingView.as_view(), name='create_booking'),
-    path('edit_user/<int:pk>/', EditBookingView.as_view(), name='edit_booking'),
+    path('edit_booking/<int:pk>/', EditBookingView.as_view(), name='edit_booking'),
+    path('delete_booking/<int:pk>/', DeleteBookingView.as_view(), name='delete_booking'),
 ]
