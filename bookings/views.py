0b8109fc3c63933e9d92c36cab7ec8ac614d08ff
GaryDolan <garydolan4@gmail.com>
2024-01-03 16:25:39 +0000

    Add edit booking view #47

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm
@@ -20,3 +21,18 @@ class CreateBookingView(LoginRequiredMixin, generic.CreateView):
     #override the get success_url method to dynamically call the correct user profile
     def get_success_url(self):
         return reverse('profile', kwargs={'username': self.request.user.username})
+    
+class EditBookingView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Booking
+    template_name = 'edit_booking.html'
+    form_class = BookingForm
+
+    # Override the get success to redirect back to the specific user profile page 
+    def get_success_url(self):
+        messages.success(self.request, 'You have successfully updated your booking.')
+        return reverse_lazy('profile', args=[self.object.owner.username])
+    
+    # Test function for UserPassesTestMixin
+    def test_func(self):
+        return self.request.user == self.get_object().owner
+    
