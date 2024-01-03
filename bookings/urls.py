dc10b7e719c5912d585ce693b6415a76f5038b7f
GaryDolan <garydolan4@gmail.com>
2024-01-03 21:01:37 +0000

    Add booking delete confirmation modal #47

--- bookings/urls.py
+++ bookings/urls.py
@@ -5,5 +5,5 @@ from .views import CreateBookingView, EditBookingView, DeleteBookingView
 urlpatterns = [
     path('create_booking/', CreateBookingView.as_view(), name='create_booking'),
     path('edit_booking/<int:pk>/', EditBookingView.as_view(), name='edit_booking'),
-    path('delete_booking/<int:pk>/', DeleteBookingView.as_view(), name='delete_booking'),
+    path('delete_booking/', DeleteBookingView.as_view(), name='delete_booking'),
 ]
