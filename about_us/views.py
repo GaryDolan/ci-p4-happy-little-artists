4919a11912a011b30e89a67aeb96222cf88ab421
GaryDolan <garydolan4@gmail.com>
2024-01-06 02:12:09 +0000

    Add about us teacher & structure sections #49

--- about_us/views.py
+++ about_us/views.py
@@ -3,4 +3,6 @@ from django.views import View
 
 class AboutUsView(View):
     def get(self, request):
-        return render(request, 'about_us.html')
+        current_page = 'about_us'
+        return render(request, 'about_us.html',{'current_page': current_page})
+
