506c41a24fb7a6c561208d13f27df37b3bf105f0
GaryDolan <garydolan4@gmail.com>
2024-01-07 01:56:31 +0000

    Add email sending based on contact form #48

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,6 +1,9 @@
+from smtplib import SMTPException
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib import messages
+from django.core.mail import send_mail
+
 from .forms import ContactUsForm
 
 class ContactUsView(View):
@@ -26,11 +29,30 @@ class ContactUsView(View):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            # Send email using form info and using EmailJS
+            # Retrieve the sender details
+            from_name = self.request.POST.get('name')
+            from_email = self.request.POST.get('email')
+            enquiry = self.request.POST.get('message')
 
-            # Redirect the user home and display a success message
-            messages.success(request, 'We have received you message and will be in touch soon.')
-            return redirect('home')
+            try:
+                # Send email using form details
+                send_mail(
+                    subject='Happy little artists enquiry',
+                    # using html message instead
+                    message='',
+                    html_message=f'<p>{enquiry}</p>\n\n<p><strong>Sent by {from_name}</strong> ({from_email})</p>',
+                    from_email=None, 
+                    recipient_list=['happylittleartistsartclub@gmail.com'],
+                )
+
+                # Redirect the user home and display a success message
+                messages.success(request, 'We have received you message and will be in touch soon.')
+                return redirect('home')
+            except SMTPException as e:
+                # Catch any SMTP-related exceptions
+                messages.error(request, 'An error occurred while sending the email. Please try again later.')
+                return redirect('home')
+        
         # form was not valid, return to form page and display message 
         messages.warning(request, 'Form invalid please try again')
         return render(request, self.template_name, {'form': form})
--- main/settings.py
+++ main/settings.py
@@ -184,3 +184,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('GMAIL_ADDRESS')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD') 
+DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_ADDRESS')
