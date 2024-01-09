b5a699d703e56c4184e35fb9681c223007430d7d
GaryDolan <garydolan4@gmail.com>
2024-01-09 00:19:50 +0000

    Edit set timeout function to trigger if msg exists

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 // Timeout function for messaging
 setTimeout(function () {
     let messages = document.getElementById('msg');
-    let alert = new bootstrap.Alert(messages);
-    alert.close();
+
+    if (messages) {
+        let alert = new bootstrap.Alert(messages);
+        
+        if (alert) {
+            alert.close();
+        }
+    }
 }, 3000);
