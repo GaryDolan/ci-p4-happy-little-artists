1a26dd2cbf920133d26be8911b00ed80c150e2f7
GaryDolan <garydolan4@gmail.com>
2024-01-09 20:29:50 +0000

    Update all site images to improve performance #40

--- main/settings.py
+++ main/settings.py
@@ -202,7 +202,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = socket.gethostbyname('smtp.gmail.com')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('GMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_ADDRESS')
